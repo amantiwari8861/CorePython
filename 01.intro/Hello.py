7d64859a4c5c6db0e0e0164309072ccb0f4b8a00
Aman Tiwari <amantiwari8861@gmail.com>
2024-07-25 19:33:47 +0530

    basic intro

---
+++ 01.intro/Hello.py
@@ -0,0 +1,30 @@
+print("hello world")
+print('hello world')
+print("""hello there """)
+    # print('''hello there ''')
+
+# single line comment
+print("hiii") # this line is printing hiii string
+
+""" print('helllo')
+print('helllo')
+print('helllo')
+print('helllo')
+print('helllo')
+print('helllo')
+print('helllo')
+print('helllo')
+print('helllo') """
+
+"""
+    1. documentation
+    2. plugability of code
+"""
+'''
+    multiple
+    line 
+    comment
+'''
+
+
+
---
+++ 01.intro/escapeSequence.py
@@ -0,0 +1,9 @@
+# print("Hello "aman" Sir ")
+# print("Hello \"aman\" Sir ")
+# print('hello \'aman sir')
+
+print("Hello",end="_") # \n
+# print("Hello",end="_")
+# print("Hello",end="_")
+# print("Hello",end="_")
+# print("Hello",end="_")
